8ce2c8ddf7bd69a6708f480102cd509c04a9a1b3
BurajeVallary <vallaryburaje@gmail.com>
2023-08-15 21:54:32 +0300

    added an image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import { getCharacters } from "./utilities/utilis";
-// import Image from "next/image";
+import Image from "next/image";
 
 interface Character {
   id: number;
