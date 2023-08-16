02b3ee3f1d6d10f471adcc4f2282f1bc2336dd36
BurajeVallary <vallaryburaje@gmail.com>
2023-08-17 00:28:32 +0300

    added the hero section

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <img
                     src="hogwarts.jpg"
                     layout="fill"
-                    objectFit="cover"
+                    objectit="cover"
                     width={1900}
                     priority
                     alt="Hero Section Background Image"
@@ -17,10 +17,12 @@ const Hero = () => {
             </div>
 
             <div className="absolute inset-0 flex items-center bg-gradient-to-r from-black via-transparent to-transparent">
-                <div className="w-full md:w-3/5 ml-8 text-center md:text-left text-white ">
+                <div className="w-full md:w-3/5 ml-8 text-center md:text-left text-white">
                     <h1 className="text-3xl md:text-6xl font-light leading-tight text-white">
                         Enter the Wizarding World:
-                       <h1> <span className="block md:inline">Unleash Your Inner Hero</span></h1>
+                        <span className="block md:inline">
+                            <span>Unleash Your Inner Hero</span>
+                        </span>
                         <span className="block md:inline">with Harry Potter</span>
                     </h1>
                     <p className="text-lg mt-6 md:mt-10 text-white">
